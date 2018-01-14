8d3295a8c7a37bd270b7b23df83e3626c9d4133b
Dorong <Dorong@DESKTOP-VOAQCFV>
2018-01-15 08:13:20 +0900

    make a game screen

--- hitTheNotes/src/hitTheNotes/HitTheNotes.java
+++ hitTheNotes/src/hitTheNotes/HitTheNotes.java
@@ -2,8 +2,11 @@ package hitTheNotes;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
@@ -40,6 +43,14 @@ public class HitTheNotes extends JFrame {
 	private ImageIcon backButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/backButtonEntered.png"));
 	private ImageIcon backButtonBasicImage = new ImageIcon(Main.class.getResource("../images/backButtonBasic.png"));
 	
+	private Image gameInfoImage = new ImageIcon(Main.class.getResource("../images/gameInfo.png")).getImage();
+	private Image judgementLineImage = new ImageIcon(Main.class.getResource("../images/judgementLine.png")).getImage();
+	private Image noteRouteImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	private Image noteRouteLineImage = new ImageIcon(Main.class.getResource("../images/noteRouteLine.png")).getImage();
+	
+	private Image noteBasicImage = new ImageIcon(Main.class.getResource("../images/noteBasic.png")).getImage();
+	
+	
 	//백그라운드 이미지를 담는 인스턴스. 메인 클래스 위치를 기반으로 해서 이미지 변수에 jpg 이미지를 담아줌
 	private Image background = new ImageIcon(Main.class.getResource("../images/intro.jpg")).getImage();
 	
@@ -60,6 +71,7 @@ public class HitTheNotes extends JFrame {
 	private int mouseX, mouseY;
 		
 	private boolean isMainScreen = false;
+	private boolean isGameScreen = false;
 	
 	ArrayList<Track> trackList = new ArrayList<Track>();
 	
@@ -353,17 +365,70 @@ public class HitTheNotes extends JFrame {
 	public void paint(Graphics g) {
 		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		screenGraphic = screenImage.getGraphics();
-		screenDraw(screenGraphic);
+		screenDraw((Graphics2D) screenGraphic);
 		g.drawImage(screenImage, 0, 0, null);
 	}
 		
-	public void screenDraw(Graphics g) {
+	public void screenDraw(Graphics2D g) {
 		g.drawImage(background, 0, 0, null);
 		//g.drawImage로 이미지, 역동적인 움직임을 보여주는 이미지를 보여주는 것이 일반적
 		if(isMainScreen)
 		{
 			g.drawImage(selectedImage, 340, 100, null);
 			g.drawImage(titleImage, 340, 70, null);
+		}
+		
+		if(isGameScreen)
+		{
+			g.drawImage(noteRouteImage, 228, 30, null);
+			g.drawImage(noteRouteImage, 332, 30, null);
+			g.drawImage(noteRouteImage, 436, 30, null);
+			g.drawImage(noteRouteImage, 540, 30, null);
+			g.drawImage(noteRouteImage, 640, 30, null);
+			g.drawImage(noteRouteImage, 744, 30, null);
+			g.drawImage(noteRouteImage, 848, 30, null);
+			g.drawImage(noteRouteImage, 952, 30, null);
+			
+			g.drawImage(noteRouteLineImage, 224, 30, null);
+			g.drawImage(noteRouteLineImage, 328, 30, null);
+			g.drawImage(noteRouteLineImage, 432, 30, null);
+			g.drawImage(noteRouteLineImage, 536, 30, null);
+			g.drawImage(noteRouteLineImage, 740, 30, null);
+			g.drawImage(noteRouteLineImage, 844, 30, null);
+			g.drawImage(noteRouteLineImage, 948, 30, null);
+			g.drawImage(noteRouteLineImage, 1052, 30, null);
+			
+			g.drawImage(gameInfoImage, 0, 660, null);
+			g.drawImage(judgementLineImage, 0, 580, null);
+			
+			g.drawImage(noteBasicImage, 228, 120, null);
+			g.drawImage(noteBasicImage, 332, 580, null);
+			g.drawImage(noteBasicImage, 436, 500, null);
+			g.drawImage(noteBasicImage, 540, 340, null);
+			g.drawImage(noteBasicImage, 640, 340, null);
+			g.drawImage(noteBasicImage, 744, 325, null);
+			g.drawImage(noteBasicImage, 848, 305, null);
+			g.drawImage(noteBasicImage, 952, 305, null);
+			
+			g.setColor(Color.white);
+			g.setRenderingHint( RenderingHints.KEY_TEXT_ANTIALIASING, 
+					RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+			g.setColor(Color.WHITE);
+			g.setFont(new Font("Arial", Font.BOLD, 30));
+			g.drawString("Joakim Karud - Mighty Love", 20, 702);
+			g.drawString("Easy", 1190, 702);
+			g.setFont(new Font("Arial", Font.PLAIN, 26));
+			g.setColor(Color.DARK_GRAY);
+			g.drawString("S", 270, 609);
+			g.drawString("D", 374, 609);
+			g.drawString("F", 478, 609);
+			g.drawString("Space Bar", 580, 609);
+			g.drawString("J", 784, 609);
+			g.drawString("K", 889, 609);
+			g.drawString("L", 993, 609);
+			g.setColor(Color.LIGHT_GRAY);
+			g.setFont(new Font("Elephant", Font.BOLD, 30));
+			g.drawString("000000", 565, 702);
 		}
 		
 		paintComponents(g); //메인프레임에 추가된 요소들을 보여줌(add 사용)
@@ -411,6 +476,7 @@ public class HitTheNotes extends JFrame {
 		
 		background = new ImageIcon(Main.class.getResource("../images/" + trackList.get(nowSelected).getGameImage())).getImage();
 		
+		isGameScreen = true;
 	}
 	
 	public void backMain() {
@@ -424,6 +490,8 @@ public class HitTheNotes extends JFrame {
 		
 		background = new ImageIcon(Main.class.getResource("../images/mainBackground2.jpg")).getImage();
 		selectTrack(nowSelected);
+		
+		isGameScreen = false;
 	}
 	
 	public void enterMain() {
